51228740c4096db84ddf54306754912da39b8b3f
Ignacio Luis Alejandro Yugdar <96100166+DiSsaOr2805@users.noreply.github.com>
2022-08-03 19:38:37 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: '.env' })
 
 const conectarDB = async () => {
     try {
-            mongoose.connect(process.env.DB_URL, {
+            await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
