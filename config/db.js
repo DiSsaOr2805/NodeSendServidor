94f7d5be12d52085dc8702bba3815347b7e1e451
Ignacio Yugdar <dissaor2805@gmail.com>
2022-08-03 19:16:08 -0300

    mongoose

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 require('dotenv').config({ path: '.env' })
 
 const conectarDB = async () => {
-
     try {
-        await mongoose.connect(process.env.DB_URL, {
+            mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
@@ -12,7 +11,7 @@ const conectarDB = async () => {
     } catch (error) {
         console.log('Hubo un error');
         console.log(error);
-        // process.exit(1)
+        process.exit(1)
     }
 }
 
