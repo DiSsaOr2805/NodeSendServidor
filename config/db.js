fe32a2a692577570159d78a210f5eddf341f0c71
Ignacio Yugdar <dissaor2805@gmail.com>
2022-08-03 19:10:11 -0300

    mongoose

--- config/db.js
+++ config/db.js
@@ -6,9 +6,7 @@ const conectarDB = async () => {
     try {
         await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify:false,
-            useCreateIndex: true
+            useUnifiedTopology: true
         })
         console.log('DB Conectada');
     } catch (error) {
